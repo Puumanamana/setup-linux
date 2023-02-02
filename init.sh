12c93eb1687d68e47b14f2f09dde789eae98595d
Cedric <carisdak@hawaii.edu>
2023-02-01 22:55:12 -0500

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,13 +5,15 @@
 # ~~~~~~~~~~~~~~~~~~~~~~~~~ #
 
 # Install zsh if not there and a few other packages (emacs, docker)
-sudo apt-get update -y && sudo apt-get install -y zsh fzy emacs docker wget git
+sudo apt-get update -y && sudo apt-get install -y zsh fzy emacs docker wget git tmux
 
 # Download oh-my-zsh
 echo y | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh | zsh
 
 # zplug for plugins
+chsh -s /bin/zsh USERNAME
+zsh
 source ~/.zplug/init.zsh
 
 # Better cd command
@@ -44,7 +46,7 @@ curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Ma
     rm -f Mamba*.sh
 
 # Python env
-mamba create -n py3 -c conda-forge -c bioconda -y pandas-gbq seaborn scipy scikit-learn scikit-survival xgboost
+mamba create -n py3 -c conda-forge -c bioconda -y pandas-gbq seaborn scipy scikit-learn scikit-survival
 # R env
 mamba create -n r -c conda-forge -c bioconda -y \
        r-remotes r-biocmanager r-tidyverse r-circlize r-survminer r-bigrquery r-ggpubr \
